fee942db56dae189183f054cfae3e09669802fa8
Jani Järvinen <jani.jarvinen@intertechno.org>
2016-11-22 09:39:21 +0200

    Updated binaries related to the IoT project.

--- PointCollegeSensorControl/PointCollegeSensorControl/Models/IotDataModel.Designer.cs
+++ PointCollegeSensorControl/PointCollegeSensorControl/Models/IotDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Codes\Syksy2015IoT\PointCollegeSensorControl\PointCollegeSensorControl\Models\IotDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\oppiadmin\Source\Repos\Syksy2015IoT\PointCollegeSensorControl\PointCollegeSensorControl\Models\IotDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
