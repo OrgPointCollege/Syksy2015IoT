2476280846c969b879968040af33375a26b694c0
Jani Järvinen <jani.jarvinen@intertechno.org>
2016-12-15 10:38:35 +0200

    Lisätty yksinkertainen käyttöliittymä XAML-sovellukseen.

--- SensorDemo/SensorDemo/MainPage.xaml.cs
+++ SensorDemo/SensorDemo/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace SensorDemo
         {
             this.InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "Hei, Raspberry!";
+        }
     }
 }
